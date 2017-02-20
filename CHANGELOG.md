964b094266aba200ec599708281385c16455b52c
Ewa Czechowska <ewa@ai-traders.com>
2017-02-20 14:41:43 +0000

    ensure the `node_modules` directory is owned by ide; add more verbosity

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+### 0.1.3 (20 Feb 2017)
+
+* ensure the `node_modules` directory is owned by ide
+* add more verbosity around the `node_modules` directory
+
 ### 0.1.2 (8 Sep 2016)
 
 * copy the `node_modules` directory instead of symlinking to it
--- image/etc_ide.d/scripts/60-ensure-node-modules.sh
+++ image/etc_ide.d/scripts/60-ensure-node-modules.sh
@@ -2,5 +2,8 @@
 
 # this must be invoked after uid:gid of ide user is changed
 if [[ -d "${ide_home}/node_modules" ]]; then
-  cp -R "${ide_home}/node_modules" "${ide_work}/node_modules"
+  (set -x; cp -R "${ide_home}/node_modules" "${ide_work}/node_modules")
+  (set -x; chown ide:ide -R "${ide_work}/node_modules")
+else
+  echo "${ide_home}/node_modules directory does not exist"
 fi
